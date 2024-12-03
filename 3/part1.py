8bf4709d0fdbb773617dae46c5b7a6a0dee43196
Mario Ebenhofer <manolol@mailbox.org>
2024-12-03 18:34:07 +0100

    3

--- 3/part1.py
+++ 3/part1.py
@@ -1,1 +1,16 @@
 import utils
+import re
+
+data = "".join(utils.load_data("task-data.txt"))
+
+mul_instructions = re.findall(r"mul\([0-9]{1,3},[0-9]{1,3}\)", data)
+
+#print(mul_instructions)
+
+sum = 0
+
+for instruction in mul_instructions:
+    factors = [int(x) for x in re.findall(r"[0-9]{1,3}", instruction)]
+    sum += factors[0] * factors[1]
+
+print(sum)
--- 3/part2.py
+++ 3/part2.py
@@ -1,1 +1,23 @@
 import utils
+import re
+
+data = "".join(utils.load_data("task-data.txt"))
+
+instructions = re.findall(r"mul\([0-9]{1,3},[0-9]{1,3}\)|do\(\)|don't\(\)", data)
+
+print(instructions)
+
+sum = 0
+mul_enabled = True
+
+for instruction in instructions:
+    if instruction == "don't()":
+        mul_enabled = False
+    elif instruction == "do()":
+        mul_enabled = True
+    else:
+        if mul_enabled:
+            factors = [int(x) for x in re.findall(r"[0-9]{1,3}", instruction)]
+            sum += factors[0] * factors[1]
+
+print(sum)
