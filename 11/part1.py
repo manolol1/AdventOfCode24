12486d04005d64ec8be4e62112a366ea77482078
Mario Ebenhofer <manolol@mailbox.org>
2024-12-11 21:32:07 +0100

    11

--- 11/part1.py
+++ 11/part1.py
@@ -1,1 +1,26 @@
 import utils
+
+blinks = 25
+
+stones = list(map(int, utils.load_data("task-data.txt")[0].split()))
+
+print(stones)
+
+for blink in range(blinks):
+    new_stones = []
+    for i in range(len(stones)):
+        stone = stones[i]
+        stone_str = str(stone)
+
+        if stone == 0:
+            new_stones.append(1)
+        elif len(stone_str) % 2 == 0:
+            new_stones.append(int(stone_str[:len(stone_str) // 2]))
+            new_stones.append(int(stone_str[len(stone_str) // 2:]))
+        else:
+            new_stones.append(stone * 2024)
+    stones = new_stones
+    #print(stones)
+    print(blink)
+
+print(len(stones))
--- 11/part2.py
+++ 11/part2.py
@@ -1,1 +1,29 @@
+# improved version of part1, using (default)dict to only store amount of stones, because the order doesn't matter
+
+from collections import defaultdict
+
 import utils
+
+blinks = 75
+
+stone_input = list(map(int, utils.load_data("task-data.txt")[0].split()))
+stone_set = set(stone_input)
+
+stones = {stone: stone_input.count(stone) for stone in stone_set}
+
+for blink in range(blinks):
+    new_stones = defaultdict(int)
+    for stone in stones:
+        stone_str = str(stone)
+        stone_count = stones[stone]
+
+        if stone == 0:
+            new_stones[1] += stone_count
+        elif len(stone_str) % 2 == 0:
+            new_stones[int(stone_str[:len(stone_str) // 2])] += stone_count
+            new_stones[int(stone_str[len(stone_str) // 2:])] += stone_count
+        else:
+            new_stones[2024 * stone] += stone_count
+    stones = new_stones
+
+print(sum(stones.values()))
