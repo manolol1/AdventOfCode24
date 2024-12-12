8048df1b4fa653e80a369e633b4526e6ea45be30
Mario Ebenhofer <manolol@mailbox.org>
2024-12-12 20:10:55 +0100

    12

--- 12/part1.py
+++ 12/part1.py
@@ -1,1 +1,9 @@
 import utils
+
+field = [list(line) for line in utils.load_data("sample-data.txt")]
+
+utils.print_matrix(field)
+
+for i, row in enumerate(field):
+    for j, col in enumerate(field[i]):
+        pass
